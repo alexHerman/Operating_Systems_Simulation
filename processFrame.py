b7ecc69968ea928d46273f623025885af2d46578
Andrew Fagrey <andrew.fagrey@gmail.com>
2016-04-30 17:35:03 -0600

    Have working code up to actually doing the scheduling

--- processFrame.py
+++ processFrame.py
@@ -14,16 +14,80 @@ class processFrame(Frame):
 		#self.initRoundRobin();
 		self.parent.mainloop()
 
-	def printSelection(self):
-		print(self.timeQuantaBox.get())
-
 	def generateProcessesList(self):
 		random.seed()
+		self.processName = []
+		self.processBurstTime = []
+		self.processPrioriy = []
+		self.processArrival = []
 		#remove all elelments in the list
 		self.processListBox.delete(0, END)
 		for i in range(0, int(self.processCountBox.get())): #maybe check that there is actually a number here
-			processName = "P" + str(i+1) + ":  " + str(random.randint(0,50)).zfill(2) + "  " + str(random.randint(1,9)) + "  " + str(i+1).zfill(2)
-			self.processListBox.insert(END, processName)
+			self.processName.append("P" + str(i+1))
+			self.processBurstTime.append(random.randint(0,50))
+			self.processPrioriy.append(random.randint(1,9))
+			self.processArrival.append(i+1)
+			processLabel = self.processName[i] + ":  " + str(self.processBurstTime[i]).zfill(2) + "  " + str(self.processPrioriy[i]) + "  " + str(self.processArrival[i]).zfill(2)
+			self.processListBox.insert(END, processLabel)
+
+
+	def roundRobinGanttChart(self):
+		# remove other frame
+		self.setProcessesFrame()
+		print("Generating round robin...")
+		# build list for processes
+		count = int(self.processCountBox.get())
+		processList = []
+
+		for i in range(0, count):
+
+			can = Canvas(self.processDisplay, width=50, height=50)
+			processList.append(can)
+			processList[i].create_rectangle(0, 0, 50, 50, fill="green")
+			processList[i].grid(row=0, column=i)
+
+	def priorityGanttChart(self):
+		# remove other frame
+		self.setProcessesFrame()
+		print("Generating priority...")
+		processName = []
+		processList = []
+		for i in range(0, 5):
+			can = Canvas(self.processDisplay, width=50, height=50)
+			processList.append(can)
+			processList[i].create_rectangle(0, 0, 50, 50, fill="yellow")
+			processList[i].grid(row=0, column=i)
+
+	def SJFGanttChart(self):
+		# remove old frame
+		self.setProcessesFrame()
+		print("Generating sjf...")
+		processName = []
+		processList = []
+		for i in range(0, 10):
+			can = Canvas(self.processDisplay, width=50, height=50)
+			processList.append(can)
+			processList[i].create_rectangle(0, 0, 50, 50, fill="blue")
+			processList[i].grid(row=0, column=i)
+
+	def printSelection(self):
+		print(self.selectedMethod.get())
+		if self.selectedMethod.get() == 0:
+			print("Showing roundRobinGanttChart")
+			self.roundRobinGanttChart()
+		elif self.selectedMethod.get() == 1:
+			self.priorityGanttChart()
+		elif self.selectedMethod.get() == 2:
+			self.SJFGanttChart()
+
+	def setProcessesFrame(self):
+		self.processDisplay.grid_remove()
+		self.processDisplay = Frame(self.dataColumnFrame, width=600, height=325)
+		self.processDisplay.grid_propagate(False);
+		self.processDisplay.grid(row=1, column=0)
+
+	def clearProcessFrame(self):
+		self.processDisplay.grid_remove()
 
 	def initUI(self):
 		# FRAME FOR PROCESS LIST ON THE LEFT SIDE OF THE WINDOW
@@ -66,16 +130,16 @@ class processFrame(Frame):
 		# variable for getting a number based on radio button choosen
 		self.selectedMethod = IntVar(master=self.parent)
 
-		# set up round robin radio button
-		self.roundRobinButton = Radiobutton(self.frameRadioButtons, command=self.printSelection, text="Round Robin", variable=self.selectedMethod, value=0)
+		# set up round robin radio button - command=self.clearProcessFrame,
+		self.roundRobinButton = Radiobutton(self.frameRadioButtons, command=self.clearProcessFrame, text="Round Robin", variable=self.selectedMethod, value=0)
 		self.roundRobinButton.grid(sticky=N, row=0, column=0)
 
 		# set up priority button (scheduling)
-		self.priorityButton = Radiobutton(self.frameRadioButtons, command=self.printSelection, text="Priority", variable=self.selectedMethod, value=1)
+		self.priorityButton = Radiobutton(self.frameRadioButtons, command=self.clearProcessFrame, text="Priority", variable=self.selectedMethod, value=1)
 		self.priorityButton.grid(sticky=N, row=0, column=1)
 
 		# set up shortest job first button
-		self.shortestJobFirstButton = Radiobutton(self.frameRadioButtons, command=self.printSelection, text="Shortest Job First", variable=self.selectedMethod, value=2)
+		self.shortestJobFirstButton = Radiobutton(self.frameRadioButtons, command=self.clearProcessFrame, text="Shortest Job First", variable=self.selectedMethod, value=2)
 		self.shortestJobFirstButton.grid(sticky=N, row=0, column=2)
 
 		# default radio button to be selected
@@ -93,7 +157,7 @@ class processFrame(Frame):
 		self.timeQuantaBox.grid(row=1, column=1)
 
 		# button for generating the process display
-		Button(self.frameRadioButtons, text="Generate", command=self.processLoop).grid(row=1, column=2)
+		Button(self.frameRadioButtons, text="Generate", command=self.printSelection).grid(row=1, column=2)
 
 	# def initRoundRobin(self):
 	# 	self.roundRobinFrame = Frame(self)
@@ -103,15 +167,3 @@ class processFrame(Frame):
 	# 	Label(self.roundRobinFrame, text="Time Quanta: ").grid(row = 0, column =  0)
 	# 	self.timeQuantaBox = Entry(self.roundRobinFrame)
 	# 	self.timeQuantaBox.grid(row = 0, column = 1)
-
-
-
-	def processLoop(self):
-		print("Generating...")
-		processName = []
-		processList = []
-		for i in range(0, 5):
-			can = Canvas(self.processDisplay, width=50, height=50)
-			processList.append(can)
-			processList[i].create_rectangle(0, 0, 50, 50, fill="blue")
-			processList[i].grid(row=0, column=i)
