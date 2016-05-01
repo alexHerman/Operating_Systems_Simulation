e81ea9d5ebb66f61ef6a21f79a2ae6908765046b
Andrew Fagrey <andrew.fagrey@gmail.com>
2016-04-30 23:09:01 -0600

    added FCFS, priority, and SJF. RR not working yet

--- processFrame.py
+++ processFrame.py
@@ -14,6 +14,79 @@ class processFrame(Frame):
 		#self.initRoundRobin();
 		self.parent.mainloop()
 
+	def sortProcessesByPriority(self):
+		sizeOfList = int(self.processCountBox.get())
+		for i in range(sizeOfList-1, 0, -1):
+			for j in range(i):
+				if self.processPrioriy[j] > self.processPrioriy[j+1]:
+					# swap process priorities
+					swap = self.processPrioriy[j]
+					self.processPrioriy[j] = self.processPrioriy[j+1]
+					self.processPrioriy[j+1] = swap
+					# swap process burst times
+					swap = self.processBurstTime[j]
+					self.processBurstTime[j] = self.processBurstTime[j+1]
+					self.processBurstTime[j+1] = swap
+					# swap process name
+					swap = self.processName[j]
+					self.processName[j] = self.processName[j+1]
+					self.processName[j+1] = swap
+					# swap process arrival time
+					swap = self.processArrival[j]
+					self.processArrival[j] = self.processArrival[j+1]
+					self.processArrival[j+1] = swap
+
+	def sortProcessesByBurstTime(self):
+		sizeOfList = int(self.processCountBox.get())
+		for i in range(sizeOfList-1, 0, -1):
+			for j in range(i):
+				if self.processBurstTime[j] > self.processBurstTime[j+1]:
+					# swap process burst times
+					swap = self.processBurstTime[j]
+					self.processBurstTime[j] = self.processBurstTime[j+1]
+					self.processBurstTime[j+1] = swap
+					# swap process priorities
+					swap = self.processPrioriy[j]
+					self.processPrioriy[j] = self.processPrioriy[j+1]
+					self.processPrioriy[j+1] = swap
+					# swap process name
+					swap = self.processName[j]
+					self.processName[j] = self.processName[j+1]
+					self.processName[j+1] = swap
+					# swap process arrival time
+					swap = self.processArrival[j]
+					self.processArrival[j] = self.processArrival[j+1]
+					self.processArrival[j+1] = swap
+
+	def sortProcessesByArrivalTime(self):
+		sizeOfList = int(self.processCountBox.get())
+		for i in range(sizeOfList-1, 0, -1):
+			for j in range(i):
+				if self.processArrival[j] > self.processArrival[j+1]:
+					# swap process arrival time
+					swap = self.processArrival[j]
+					self.processArrival[j] = self.processArrival[j+1]
+					self.processArrival[j+1] = swap
+					# swap process burst times
+					swap = self.processBurstTime[j]
+					self.processBurstTime[j] = self.processBurstTime[j+1]
+					self.processBurstTime[j+1] = swap
+					# swap process priorities
+					swap = self.processPrioriy[j]
+					self.processPrioriy[j] = self.processPrioriy[j+1]
+					self.processPrioriy[j+1] = swap
+					# swap process name
+					swap = self.processName[j]
+					self.processName[j] = self.processName[j+1]
+					self.processName[j+1] = swap
+		print (str(self.processArrival))
+
+
+	def addBurstTimes(self):
+		self.burstTimeTotal = 0
+		for i in range(int(self.processCountBox.get())):
+			self.burstTimeTotal = self.burstTimeTotal + self.processBurstTime[i]
+
 	def generateProcessesList(self):
 		random.seed()
 		self.processName = []
@@ -24,11 +97,20 @@ class processFrame(Frame):
 		self.processListBox.delete(0, END)
 		for i in range(0, int(self.processCountBox.get())): #maybe check that there is actually a number here
 			self.processName.append("P" + str(i+1))
-			self.processBurstTime.append(random.randint(0,50))
-			self.processPrioriy.append(random.randint(1,9))
-			self.processArrival.append(i+1)
+			self.processBurstTime.append(int(random.randint(1,40)))
+			self.processPrioriy.append(int(random.randint(1,9)))
+			self.processArrival.append(int(i+1))
 			processLabel = self.processName[i] + ":  " + str(self.processBurstTime[i]).zfill(2) + "  " + str(self.processPrioriy[i]) + "  " + str(self.processArrival[i]).zfill(2)
 			self.processListBox.insert(END, processLabel)
+
+	def checkBurstListForValues(self):
+		count = int(self.processCountBox.get())
+		for i in range(0, count):
+			if self.copyList[i] > 0:
+				return False
+		return True
+
+
 
 
 	def roundRobinGanttChart(self):
@@ -37,48 +119,84 @@ class processFrame(Frame):
 		print("Generating round robin...")
 		# build list for processes
 		count = int(self.processCountBox.get())
+		self.timeQuanta
 		processList = []
 
-		for i in range(0, count):
+		self.generateProcessesList()
+		self.copyList = self.processBurstTime
+		hasListZero = self.checkBurstListForValues()
+		row = 0;
 
-			can = Canvas(self.processDisplay, width=50, height=50)
-			processList.append(can)
-			processList[i].create_rectangle(0, 0, 50, 50, fill="green")
-			processList[i].grid(row=0, column=i)
+		while hasListZero == False:
+			for i in range(0, count):
+				Label(self.processDisplay, text=self.processName[i]).grid(row=row, column=i)
+				width = (550 / self.copyList[i]) * self.copyList[i]
+				can = Canvas(self.processDisplay, width=self.timeQuanta, height=50)
+				processList.append(can)
+				processList[i].create_rectangle(0, 0, self.timeQuanta, 50, fill="green")
+				processList[i].grid(row=row+1, column=i)
+				row = row + 1
+				hasListZero = self.checkBurstListForValues()
+
+
+
 
 	def priorityGanttChart(self):
 		# remove other frame
 		self.setProcessesFrame()
-		print("Generating priority...")
-		processName = []
+		count = int(self.processCountBox.get())
+		# sort list by priority
+		self.sortProcessesByPriority()
+		self.addBurstTimes()
 		processList = []
-		for i in range(0, 5):
-			can = Canvas(self.processDisplay, width=50, height=50)
-			processList.append(can)
-			processList[i].create_rectangle(0, 0, 50, 50, fill="yellow")
-			processList[i].grid(row=0, column=i)
+		for i in range(0, count):
+			Label(self.processDisplay, text=self.processName[i]).grid(row=0, column=i)
+			width = (550 / self.burstTimeTotal) * self.processBurstTime[i]
+			shapeCan = Canvas(self.processDisplay, width=width, height=50)
+			processList.append(shapeCan)
+			processList[i].create_rectangle(0, 0, width, 50, fill="yellow")
+			processList[i].grid(row=1, column=i, sticky=W)
+
+	def FCFSGanttChart(self):
+		# remove other frame
+		self.setProcessesFrame()
+		count = int(self.processCountBox.get())
+		# sort list by priority
+		self.sortProcessesByArrivalTime()
+		self.addBurstTimes()
+		processList = []
+		for i in range(0, count):
+			Label(self.processDisplay, text=self.processName[i]).grid(row=0, column=i)
+			width = (550 / self.burstTimeTotal) * self.processBurstTime[i]
+			shapeCan = Canvas(self.processDisplay, width=width, height=50)
+			processList.append(shapeCan)
+			processList[i].create_rectangle(0, 0, width, 50, fill="red")
+			processList[i].grid(row=1, column=i, sticky=W)
 
 	def SJFGanttChart(self):
 		# remove old frame
 		self.setProcessesFrame()
-		print("Generating sjf...")
-		processName = []
+		count = int(self.processCountBox.get())
+		self.sortProcessesByBurstTime()
+		self.addBurstTimes()
 		processList = []
-		for i in range(0, 10):
-			can = Canvas(self.processDisplay, width=50, height=50)
-			processList.append(can)
-			processList[i].create_rectangle(0, 0, 50, 50, fill="blue")
-			processList[i].grid(row=0, column=i)
+		for i in range(0, count):
+			Label(self.processDisplay, text=self.processName[i]).grid(row=0, column=i)
+			width = (550 / self.burstTimeTotal) * self.processBurstTime[i]
+			shapeCan = Canvas(self.processDisplay, width=width, height=50)
+			processList.append(shapeCan)
+			processList[i].create_rectangle(0, 0, width, 50, fill="pink")
+			processList[i].grid(row=1, column=i, sticky=W)
 
 	def printSelection(self):
-		print(self.selectedMethod.get())
 		if self.selectedMethod.get() == 0:
-			print("Showing roundRobinGanttChart")
 			self.roundRobinGanttChart()
 		elif self.selectedMethod.get() == 1:
 			self.priorityGanttChart()
 		elif self.selectedMethod.get() == 2:
 			self.SJFGanttChart()
+		elif self.selectedMethod.get() == 3:
+			self.FCFSGanttChart()
 
 	def setProcessesFrame(self):
 		self.processDisplay.grid_remove()
@@ -134,13 +252,18 @@ class processFrame(Frame):
 		self.roundRobinButton = Radiobutton(self.frameRadioButtons, command=self.clearProcessFrame, text="Round Robin", variable=self.selectedMethod, value=0)
 		self.roundRobinButton.grid(sticky=N, row=0, column=0)
 
-		# set up priority button (scheduling)
-		self.priorityButton = Radiobutton(self.frameRadioButtons, command=self.clearProcessFrame, text="Priority", variable=self.selectedMethod, value=1)
-		self.priorityButton.grid(sticky=N, row=0, column=1)
 
 		# set up shortest job first button
 		self.shortestJobFirstButton = Radiobutton(self.frameRadioButtons, command=self.clearProcessFrame, text="Shortest Job First", variable=self.selectedMethod, value=2)
-		self.shortestJobFirstButton.grid(sticky=N, row=0, column=2)
+		self.shortestJobFirstButton.grid(sticky=N, row=0, column=1)
+
+		# set up priority button (scheduling)
+		self.priorityButton = Radiobutton(self.frameRadioButtons, command=self.clearProcessFrame, text="Priority", variable=self.selectedMethod, value=1)
+		self.priorityButton.grid(sticky=N, row=0, column=2)
+
+		# set up button for FCFS
+		self.FCFSButton = Radiobutton(self.frameRadioButtons, command=self.clearProcessFrame, text="FCFS", variable=self.selectedMethod, value=3)
+		self.FCFSButton.grid(sticky=N, row=0, column=3)
 
 		# default radio button to be selected
 		self.roundRobinButton.select()
@@ -157,7 +280,7 @@ class processFrame(Frame):
 		self.timeQuantaBox.grid(row=1, column=1)
 
 		# button for generating the process display
-		Button(self.frameRadioButtons, text="Generate", command=self.printSelection).grid(row=1, column=2)
+		Button(self.frameRadioButtons, text="Display Gantt Chart", command=self.printSelection).grid(row=1, column=3)
 
 	# def initRoundRobin(self):
 	# 	self.roundRobinFrame = Frame(self)
