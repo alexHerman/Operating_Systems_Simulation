ca5bc398c13183044cad7358de79d30470219389
Andrew Fagrey <andrew.fagrey@gmail.com>
2016-05-04 15:54:41 -0600

    Added gantt chart numbering, comments. All done eccept for RR gantt chart numbers

--- processFrame.py
+++ processFrame.py
@@ -1,7 +1,17 @@
 from Tkinter import *
 import random
 
+###########################################################################
+# Author: Alex Herman, Andrew Fagrey
+# Description: Class that contains the code for the process scheduler part
+#  of the 4th CSC456 program.
+###########################################################################
 class processFrame(Frame):
+
+	###########################################################################
+	# Author: Alex Herman, Andrew Fagrey
+	# Description: This function is the class constructor
+	###########################################################################
 	def __init__(self):
 		self.parent = Tk()
 		self.parent.title("Process Management Algorithms")
@@ -9,11 +19,15 @@ class processFrame(Frame):
 		Frame.__init__(self, self.parent)
 		self.grid()
 
-
 		self.initUI();
-		#self.initRoundRobin();
 		self.parent.mainloop()
+	############################# END OF FUNCTION #############################
 
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: This function is responsible for sorting the process lists
+	#  by their priority
+	###########################################################################
 	def sortProcessesByPriority(self):
 		sizeOfList = int(self.processCountBox.get())
 		for i in range(sizeOfList-1, 0, -1):
@@ -35,7 +49,13 @@ class processFrame(Frame):
 					swap = self.processArrival[j]
 					self.processArrival[j] = self.processArrival[j+1]
 					self.processArrival[j+1] = swap
+	############################# END OF FUNCTION #############################
 
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: This function is responsible for sorting the process lists
+	#  by their burst times
+	###########################################################################
 	def sortProcessesByBurstTime(self):
 		sizeOfList = int(self.processCountBox.get())
 		for i in range(sizeOfList-1, 0, -1):
@@ -57,7 +77,13 @@ class processFrame(Frame):
 					swap = self.processArrival[j]
 					self.processArrival[j] = self.processArrival[j+1]
 					self.processArrival[j+1] = swap
+	############################# END OF FUNCTION #############################
 
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: This function is responsible for sorting the process lists
+	#  by their arrival time
+	###########################################################################
 	def sortProcessesByArrivalTime(self):
 		sizeOfList = int(self.processCountBox.get())
 		for i in range(sizeOfList-1, 0, -1):
@@ -79,14 +105,23 @@ class processFrame(Frame):
 					swap = self.processName[j]
 					self.processName[j] = self.processName[j+1]
 					self.processName[j+1] = swap
-		print (str(self.processArrival))
+	############################# END OF FUNCTION #############################
 
-
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: This function adds the process burst times and stores the
+	#  total in a global variable
+	###########################################################################
 	def addBurstTimes(self):
 		self.burstTimeTotal = 0
 		for i in range(int(self.processCountBox.get())):
 			self.burstTimeTotal = self.burstTimeTotal + self.processBurstTime[i]
+	############################# END OF FUNCTION #############################
 
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: This function generates the process list
+	###########################################################################
 	def generateProcessesList(self):
 		random.seed()
 		self.processName = []
@@ -102,60 +137,75 @@ class processFrame(Frame):
 			self.processArrival.append(int(i+1))
 			processLabel = self.processName[i] + ":  " + str(self.processBurstTime[i]).zfill(2) + "  " + str(self.processPrioriy[i]) + "  " + str(self.processArrival[i]).zfill(2)
 			self.processListBox.insert(END, processLabel)
+	############################# END OF FUNCTION #############################
 
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: This function checks that a copy of the burst list still has
+	#  values that are greater than zero in it
+	###########################################################################
 	def checkBurstListForValues(self):
 		count = int(self.processCountBox.get())
 		for i in range(0, count):
 			if self.roundRobinBurstList[i] > 0:
 				return False
 		return True
+	############################# END OF FUNCTION #############################
 
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: A function to copy the processBurstTime list to another list
+	###########################################################################
 	def copyBurstList(self):
 		count = int(self.processCountBox.get())
 		for i in range(0, count):
 			self.roundRobinBurstList.append(self.processBurstTime[i])
+	############################# END OF FUNCTION #############################
 
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: Function for generating the gantt chart for Priority Sch.
+	###########################################################################
 	def roundRobinGanttChart(self):
-		print("starting round robin...")
+		# clear the frames
 		self.setProcessesFrame()
+
 		# get time quanta
 		quanta = int(self.timeQuantaBox.get())
+
 		# make copy of burst time list
 		self.roundRobinBurstList = []
 		self.copyBurstList()
-		#print("roundRobinBurstList")
-		#print(self.roundRobinBurstList)
+
 		# build the round robin list
 		roundRobinBurstQueue = []
 		roundRobinNameQueue = []
+
+		# set up initial loop condition variable
 		quitLoop = self.checkBurstListForValues()
+
+		# variables used in the loop below
 		width = 0
 		row = 0
 		col = 0
-		totalSize = 0
-		counter = 0
+		totalTime = 0
 		processList = []
+		colT = 0
+
+		# keep looping through the burst list till all values are less then zero
 		while quitLoop == False:
 			for i in range(0, int(self.processCountBox.get())):
-				print("BurstList[i] before:  "+str(self.roundRobinBurstList[i]))
 				if self.roundRobinBurstList[i] > 0:
 					leftOver = self.roundRobinBurstList[i] - quanta
 					if leftOver <= 0:
 						width = self.roundRobinBurstList[i]
 					else:
 						width = quanta
-					#totalSize = totalSize + width
-
-					print("width:  " + str(width))
 					roundRobinNameQueue.append(self.processName[i])
-					print("Process name:  "+str(roundRobinNameQueue[i]))
 					self.roundRobinBurstList[i] = self.roundRobinBurstList[i] - quanta
-					print("Burst After:  " + str(self.roundRobinBurstList[i])+"\n")
-
 					# display shapes and calculate size
 					Label(self.processDisplay, text=roundRobinNameQueue[i]).grid(row=row, column=col)
 					shapeCan = Canvas(self.processDisplay, width=width+1, height=50)
-					#processList.append(shapeCan)
 					shapeCan.create_rectangle(0, 0, width+1, 50, fill="green")
 					shapeCan.grid(row=row+1, column=col)
 					# update row and column
@@ -165,65 +215,150 @@ class processFrame(Frame):
 					else:
 						col = col + 1
 
+					# display the label for burst time
+					#Label(self.processDisplay, text=str(totalTime)).grid(row=row+2, column=col)
+
 			quitLoop = self.checkBurstListForValues()
-			print("----------------------------------------------------------")
-		print("name queue")
-		print(roundRobinNameQueue)
+	############################# END OF FUNCTION #############################
 
-
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: Function for generating the gantt chart for Priority Sch.
+	###########################################################################
 	def priorityGanttChart(self):
-		print("priority...")
-		# remove other frame
+		# clear the window of other gantt charts
 		self.setProcessesFrame()
-		count = int(self.processCountBox.get())
+
 		# sort list by priority
 		self.sortProcessesByPriority()
+
+		# add the burst times (used for calculating the width of the shapes)
 		self.addBurstTimes()
+
+		# some variables used below in the loop
 		processList = []
-		print("burst: " + str(self.processBurstTime))
-		for i in range(0, count):
-			Label(self.processDisplay, text=self.processName[i]).grid(row=0, column=i)
-			width = (550 / self.burstTimeTotal) * self.processBurstTime[i]
+		col = 0
+		totalTime = 0
+
+		# loop through the number of processes and display the gantt chart
+		for i in range(0, int(self.processCountBox.get())):
+			# show the process label
+			Label(self.processDisplay, text=self.processName[i]).grid(row=0, column=col+1)
+
+			# calculate the width of the shape
+			width = (300 / self.burstTimeTotal) * self.processBurstTime[i]
+
+			# add to the shape the process list and display it in the window
 			shapeCan = Canvas(self.processDisplay, width=width, height=50)
 			processList.append(shapeCan)
 			processList[i].create_rectangle(0, 0, width, 50, fill="yellow")
-			processList[i].grid(row=1, column=i, sticky=W)
+			processList[i].grid(row=1, column=col+1, sticky=W)
 
+			# display the label for burst time
+			Label(self.processDisplay, text=str(totalTime)).grid(row=2, column=col)
+
+			# get the total time so far and increase column value
+			totalTime = totalTime + self.processBurstTime[i]
+			col = col + 2
+		# End of for loop
+
+		# add remaining burst time label to window
+		Label(self.processDisplay, text=str(totalTime)).grid(row=2, column=col)
+	############################# END OF FUNCTION #############################
+
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: Function for generating the gantt chart for FCFS
+	###########################################################################
 	def FCFSGanttChart(self):
-		print("FCFS...")
-		# remove other frame
+		# clear the window of other gantt charts
 		self.setProcessesFrame()
-		count = int(self.processCountBox.get())
-		# sort list by priority
+
+		# sort list by arrival time
 		self.sortProcessesByArrivalTime()
+
+		# add the burst times (used for calculating the width of the shapes)
 		self.addBurstTimes()
+
+		# some variables used below in the loop
 		processList = []
-		print("burst: " + str(self.processBurstTime))
-		for i in range(0, count):
-			Label(self.processDisplay, text=self.processName[i]).grid(row=0, column=i)
-			width = (550 / self.burstTimeTotal) * self.processBurstTime[i]
+		col = 0
+		totalTime = 0
+
+		# loop through the number of processes and display the gantt chart
+		for i in range(0, int(self.processCountBox.get())):
+			# show the process label
+			Label(self.processDisplay, text=self.processName[i]).grid(row=0, column=col+1)
+
+			# calculate the width of the shape
+			width = (300 / self.burstTimeTotal) * self.processBurstTime[i]
+
+			# add to the shape the process list and display it in the window
 			shapeCan = Canvas(self.processDisplay, width=width, height=50)
 			processList.append(shapeCan)
 			processList[i].create_rectangle(0, 0, width, 50, fill="red")
-			processList[i].grid(row=1, column=i, sticky=W)
+			processList[i].grid(row=1, column=col+1, sticky=W)
 
+			# display the label for burst time
+			Label(self.processDisplay, text=str(totalTime)).grid(row=2, column=col)
+
+			# get the total time so far and increase column value
+			totalTime = totalTime + self.processBurstTime[i]
+			col = col + 2
+		# End of for loop
+
+		# add remaining burst time label to window
+		Label(self.processDisplay, text=str(totalTime)).grid(row=2, column=col)
+	############################# END OF FUNCTION #############################
+
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: Function for generating the gantt chart for SJF
+	###########################################################################
 	def SJFGanttChart(self):
-		print("SJF...")
-		# remove old frame
+		# clear the window of other gantt charts
 		self.setProcessesFrame()
-		count = int(self.processCountBox.get())
+
+		# sort the processes by burst time and calculate total burst time
 		self.sortProcessesByBurstTime()
 		self.addBurstTimes()
+
+		# vars used in loop below
 		processList = []
-		print("burst: " + str(self.processBurstTime))
-		for i in range(0, count):
-			Label(self.processDisplay, text=self.processName[i]).grid(row=0, column=i)
-			width = (550 / self.burstTimeTotal) * self.processBurstTime[i]
+		col = 0
+		totalTime = 0
+
+		# loop through the processes and display the gantt chart
+		for i in range(0, int(self.processCountBox.get())):
+			# show the process label
+			Label(self.processDisplay, text=self.processName[i]).grid(row=0, column=col+1)
+
+			# calculate the width of the shape
+			width = (300 / self.burstTimeTotal) * self.processBurstTime[i]
+
+			# add to the shape the process list and display it in the window
 			shapeCan = Canvas(self.processDisplay, width=width, height=50)
 			processList.append(shapeCan)
-			processList[i].create_rectangle(0, 0, width, 50, fill="pink")
-			processList[i].grid(row=1, column=i, sticky=W)
+			processList[i].create_rectangle(0, 0, width, 50, fill="blue")
+			processList[i].grid(row=1, column=col+1, sticky=W)
 
+			# display the label for burst time
+			Label(self.processDisplay, text=str(totalTime)).grid(row=2, column=col)
+
+			# get the total time so far and increase column value
+			totalTime = totalTime + self.processBurstTime[i]
+			col = col + 2
+		# End of for loop
+
+		# add remaining burst time label to window
+		Label(self.processDisplay, text=str(totalTime)).grid(row=2, column=col)
+	############################# END OF FUNCTION #############################
+
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: A function to select (based on the radio button selected)
+	#  which process scheduling algorithm to run
+	###########################################################################
 	def printSelection(self):
 		if self.selectedMethod.get() == 0:
 			self.roundRobinGanttChart()
@@ -233,16 +368,32 @@ class processFrame(Frame):
 			self.SJFGanttChart()
 		elif self.selectedMethod.get() == 3:
 			self.FCFSGanttChart()
+	############################# END OF FUNCTION #############################
 
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: A function to set the processes display frame
+	###########################################################################
 	def setProcessesFrame(self):
 		self.processDisplay.grid_remove()
 		self.processDisplay = Frame(self.dataColumnFrame, width=600, height=325)
 		self.processDisplay.grid_propagate(False);
 		self.processDisplay.grid(row=1, column=0)
+	############################# END OF FUNCTION #############################
 
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: A function for removing the process display frame
+	###########################################################################
 	def clearProcessFrame(self):
 		self.processDisplay.grid_remove()
+	############################# END OF FUNCTION #############################
 
+	###########################################################################
+	# Author: Andrew Fagrey
+	# Description: This function is responsible for laying out the frames for
+	#  the various elements of the process scheduling UI
+	###########################################################################
 	def initUI(self):
 		# FRAME FOR PROCESS LIST ON THE LEFT SIDE OF THE WINDOW
 		# set up left side column for processes
@@ -252,7 +403,6 @@ class processFrame(Frame):
 
 		# process list
 		# show label and entry box for process count
-		#self.processCount = 0
 		Label(self.processListColumnFrame, text="Enter process count: ").grid(row=0, column=0, sticky=W)
 		self.processCountBox = Entry(self.processListColumnFrame)
 		self.processCountBox.grid(row=1, column=0, sticky=W)
@@ -316,12 +466,4 @@ class processFrame(Frame):
 
 		# button for generating the process display
 		Button(self.frameRadioButtons, text="Display Gantt Chart", command=self.printSelection).grid(row=1, column=3)
-
-	# def initRoundRobin(self):
-	# 	self.roundRobinFrame = Frame(self)
-	# 	self.roundRobinFrame.grid(row = 1, column = 2)
-	# 	self.timeQuanta = StringVar()
-	#
-	# 	Label(self.roundRobinFrame, text="Time Quanta: ").grid(row = 0, column =  0)
-	# 	self.timeQuantaBox = Entry(self.roundRobinFrame)
-	# 	self.timeQuantaBox.grid(row = 0, column = 1)
+	############################# END OF FUNCTION #############################
